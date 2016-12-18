e94770c4dcc3cdc718ff4cf905d4561c662fdffe
Matt Thompson <mattthompson34@yahoo.co.uk>
2016-12-18 20:31:04 +0000

    Convert DELETE todos to sequelize

--- server.js
+++ server.js
@@ -64,21 +64,25 @@ app.post('/todos', function(req, res) {
 	});
 });
 
-// DELETE /todos/:id
+// DELETE /todos/:id (sequelize)
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (!matchedTodo) {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function (rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function () {
+		res.status(500).send();
+	});
 });
 
 // PUT /todo/:id
