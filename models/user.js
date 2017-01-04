ecf7c6ba4c033ae249cf02fb7a4bf3ea6fc03f80
Matt Thompson <mattthompson34@yahoo.co.uk>
2017-01-04 22:02:16 +0000

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcryptjs');
+var _ = require('underscore');
+
 module.exports = function (sequelize, DataTypes) {
 	return sequelize.define('user', {
 		email: {
@@ -8,11 +11,25 @@ module.exports = function (sequelize, DataTypes) {
 				isEmail: true
 			}
 		},
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
 		password: {
-			type: DataTypes.STRING,
+			type: DataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
 				len: [7, 100]
+			},
+			set: function (value) {		// set password with the value of the password
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);	// password still won't be stored in the db as it's a virtual DataType
+				this.setDataValue('salt', salt);		// but now the salt will be stored in the db (so we can log users in)
+				this.setDataValue('password_hash', hashedPassword);
 			}
 		}
 	}, {
@@ -22,6 +39,16 @@ module.exports = function (sequelize, DataTypes) {
 					user.email = user.email.toLowerCase();
 				}
 			}
+		},
+		instanceMethods: {
+			toPublicJSON: function () {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			}
 		}
 	});
 }
+
+
+// app.post('/partners', function(req, res) {
+//     var body = _.pick(req.body, 'email', 'name', 'position', 'office');
